d479794f2f9abdf879c890436d2f8e4d354cb9cf
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2022-04-02 10:30:39 +0900

    課題4-1を追加

---
+++ fundation/演習用ソースコード（UTF-8）/Ex1_04_1.java
@@ -0,0 +1,1 @@
+class Ex1_04_1 {public static void main (String[] args) {for(int i = 1 ; i <= 9  ; i++){String disp3 = "";for(int j = 1 ; j <= 9  ; j++){disp3 = disp3 + i*j + " ";}if(args.length == 1){if(i == Integer.parseInt(args[0])){System.out.println(disp3);}}else{System.out.println(disp3);}}}}
