8938db8012e330bcdb6a653e7e134e799deda566
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2022-04-02 10:39:12 +0900

    Ex1_04_1に解答

--- fundation/演習用ソースコード（UTF-8）/Ex1_04_1.java
+++ fundation/演習用ソースコード（UTF-8）/Ex1_04_1.java
@@ -1,1 +1,21 @@
-class Ex1_04_1 {public static void main (String[] args) {for(int i = 1 ; i <= 9  ; i++){String disp3 = "";for(int j = 1 ; j <= 9  ; j++){disp3 = disp3 + i*j + " ";}if(args.length == 1){if(i == Integer.parseInt(args[0])){System.out.println(disp3);}}else{System.out.println(disp3);}}}}
+/*-< 演習：Ex1_04_1 >---------------------------------
+九九を表示するプログラムです。
+数値を一つ入力して実行することで入力した数の段のみを表示することができます。
+数値を入力せずに実行した場合は全ての段が表示されます。
+----------------------------------------------------*/
+class Ex1_04_1 {
+    public static void main (String[] args) {
+        for(int i = 1 ; i <= 9  ; i++){
+            String disp3 = "";
+            for(int j = 1 ; j <= 9  ; j++){
+                disp3 = disp3 + i*j + " ";
+            }if(args.length == 1){
+                if(i == Integer.parseInt(args[0])){
+                    System.out.println(disp3);
+                }
+            }else{
+                System.out.println(disp3);
+            }
+        }
+    }
+}
