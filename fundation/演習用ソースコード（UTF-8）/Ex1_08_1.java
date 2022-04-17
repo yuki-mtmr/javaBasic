af4a074703d20063919c7d8c0653d8f6466ed739
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2022-04-17 10:05:14 +0900

    chore:ex8に解答

--- fundation/演習用ソースコード（UTF-8）/Ex1_08_1.java
+++ fundation/演習用ソースコード（UTF-8）/Ex1_08_1.java
@@ -8,14 +8,14 @@ class Ex1_08_1 {
 		**     配列名    ：names
 		**     保持する値：「taro」「takeshi」「hanako」「moco」「satoshi」
 		*/
-		
+		String[] names = {"taro", "takeshi", "hanako", "moco", "satoshi"};
 		
 		
 		
 		/* (2) 以下のアンダーバーを埋め、
 		**     names配列から「moco」の値を表示するプログラムに修正してください。
 		*/
-		String printName = ______ ;         //(21行目)アンダーバーに適切な処理を埋めてください
+		String printName = names[3] ;         //(21行目)アンダーバーに適切な処理を埋めてください
 		System.out.println( printName );    //(22行目)変更しないでください
 		
 	}
--- fundation/演習用ソースコード（UTF-8）/Ex1_08_3.java
+++ fundation/演習用ソースコード（UTF-8）/Ex1_08_3.java
@@ -8,9 +8,9 @@
 class Ex1_08_3 {
 	public static void main (String[] args) {
 		
-		int input1 = ______________ ;                      //(11行目)アンダーバーに適切な処理を埋めてください
-		int input2 = ______________ ;                      //(12行目)アンダーバーに適切な処理を埋めてください
-		int input3 = ______________ ;                      //(13行目)アンダーバーに適切な処理を埋めてください
+		int input1 = Integer.parseInt(args[0]) ;                      //(11行目)アンダーバーに適切な処理を埋めてください
+		int input2 = Integer.parseInt(args[1]) ;                      //(12行目)アンダーバーに適切な処理を埋めてください
+		int input3 = Integer.parseInt(args[2]) ;                      //(13行目)アンダーバーに適切な処理を埋めてください
 		                                                   
 		System.out.println( input1 + input2 + input3 );    //(15行目)変更しないでください
 		
